749312091dd9c07a40580b2eb40d79d0412807bc
Robert Carroll <rpcarroll95@gmail.com>
2016-05-10 19:22:14 -0400

    first commit

---
+++ src/main/java/io/intellijokers/App.java
@@ -0,0 +1,7 @@
+package io.intellijokers;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/io/intellijokers/AppTest.java
@@ -0,0 +1,7 @@
+package io.intellijokers;
+
+public class AppTest {
+    /**
+     * THIS CAN BE DELETED. BEING USED TO PUSH THE TEST FOLDER UP WITHOUT IT BEING HIDDEN
+     */
+}
