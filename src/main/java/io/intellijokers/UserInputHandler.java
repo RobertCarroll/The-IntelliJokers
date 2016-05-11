764afaff58c737d6a31c600132616e1f299353fc
Michael Cane <michaelcane@Zipcoder06s-MacBook-Pro.local>
2016-05-11 15:45:28 -0400

    game & horse

---
+++ src/main/java/io/intellijokers/UserInputHandler.java
@@ -0,0 +1,17 @@
+package io.intellijokers;
+
+import java.util.Scanner;
+
+public class UserInputHandler {
+
+    Scanner input = new Scanner(System.in);
+
+    public void promptTheUserForAString(String msg) {
+        promptUser(msg);
+        return 
+    }
+
+    public void promptUser(String msg) {
+        System.out.println(msg);
+    }
+}
