28810644d6c7be859d2c87337ecad9a6307e6348
Dimitri Vaughn <dimitrivaughn@Zipcoder27s-MacBook-Pro.local>
2016-05-11 21:53:10 -0400

    Adding Roulette Class

---
+++ src/main/java/io/intellijokers/Roulette.java
@@ -0,0 +1,48 @@
+package io.dimitrivaughn;
+
+/**
+ * Created by dimitrivaughn on 5/11/16.
+ */
+public class Roulette {
+
+    private int wheelNumber;
+
+    public void takeBet(){}
+
+    public void setWheelNumber(){
+        wheelNumber = (int) (Math.random()*36);
+    }
+
+    public int getWheelNumber (){
+        return wheelNumber;
+    }
+
+    public void payPlayer(){}
+
+    public void determineWinner(){}
+
+
+
+
+
+
+    public void greetPlayer(){}
+
+    public void printBettingOptions(){}
+
+    public void giveHistory1(){}
+
+    public void runHistoryLoop(){}
+
+    public void printGameManual(){}
+
+    public void rouletteEngine(){}
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
