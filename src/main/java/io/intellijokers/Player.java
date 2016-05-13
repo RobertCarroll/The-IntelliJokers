53ecde0586365de788fa1118158b2f45c4d08639
Robert Carroll <rpcarroll95@gmail.com>
2016-05-13 13:50:21 -0400

    update player for taking bets

--- src/main/java/io/intellijokers/Player.java
+++ src/main/java/io/intellijokers/Player.java
@@ -1,23 +1,34 @@
 package io.intellijokers;
 
+import java.util.ArrayList;
+
 /**
  * Created by alejandrolondono on 5/11/16.
  */
 public class Player extends User{
 
-    double cash;
+    int cash = 0;
+    ArrayList<Integer> bets;
 
-    public double getCash() {
+    public int getCash() {
         return cash;
     }
 
-    public void setCash(double cash) {
+    public void setCash(int cash) {
         this.cash = cash;
     }
 
+    public int placeBet(int amount){
+        this.setCash(this.getCash()-amount);
+        return amount;
+    }
 
+    public int placeBet(int amount, ArrayList<Integer> bets){
+        this.bets = bets;
+        return placeBet(amount);
+    }
 
-    public Player(String name, double money){
+    public Player(String name, int money){
         super(name);
         this.setCash(money);
     }
