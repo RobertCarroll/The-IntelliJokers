67c60fd167f84ec34154b54b00a7c7f6c851e063
Robert Carroll <rpcarroll95@gmail.com>
2016-05-15 17:12:39 -0400

    fixed compile error

--- src/main/java/io/intellijokers/App.java
+++ src/main/java/io/intellijokers/App.java
@@ -1,7 +1,7 @@
 package io.intellijokers;
 
 public class App {
-    Casino casino= new Casino();
+    Casino casino = new Casino();
     public static void main(String[] args) {
         App app = new App();
         app.casino.enterCasino();
